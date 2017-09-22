7742611d08b5ef3bb3ea6b87765fb163d071a83d
sergi.dote <anna1106>
2017-09-22 13:56:09 +0200

    changes

--- src/app/models/app.model.ts
+++ src/app/models/app.model.ts
@@ -3,51 +3,57 @@ export type CardName = 'GOOGLE' | 'FACEBOOK' | 'APPLE' | 'MICROSOFT' | 'AMAZON';
 export interface StockData {
     name: CardName;
     logo: string;
-    marketCapital: number;
+    marketCapital: number | string;
     openValue: number;
     minValue: number;
     maxValue: number;
+    currentValue: number;
 }
 
 export const stockDataList: StockData[] = [
     {
         name: 'GOOGLE',
         logo: 'assets/google.png',
-        marketCapital: 656537958854,
+        marketCapital: '650.99B',
         openValue: 948.13,
         minValue: 948.13,
-        maxValue: 952.8
+        maxValue: 952.8,
+        currentValue: 952.8
     },
     {
         name: 'FACEBOOK',
         logo: 'assets/facebook.png',
-        marketCapital: 656537958854,
+        marketCapital: '650.99B',
         openValue: 948.13,
         minValue: 948.13,
-        maxValue: 952.8
+        maxValue: 952.8,
+        currentValue: 952.8
     },
     {
         name: 'AMAZON',
         logo: 'assets/amazon.jpg',
-        marketCapital: 656537958854,
+        marketCapital: '650.99B',
         openValue: 948.13,
         minValue: 948.13,
-        maxValue: 952.8
+        maxValue: 952.8,
+        currentValue: 952.8
     },
     {
         name: 'APPLE',
         logo: 'assets/apple.png',
-        marketCapital: 656537958854,
+        marketCapital: '650.99B',
         openValue: 948.13,
         minValue: 948.13,
-        maxValue: 952.8
+        maxValue: 952.8,
+        currentValue: 952.8
     },
     {
         name: 'MICROSOFT',
         logo: 'assets/microsoft.jpg',
-        marketCapital: 656537958854,
+        marketCapital: '650.99B',
         openValue: 948.13,
         minValue: 948.13,
-        maxValue: 952.8
+        maxValue: 952.8,
+        currentValue: 952.8
     }
 ];
